cf69cc5b47e5b8cbdcbd7551f6c837e0414ff16d
Willzi3 <williamszachary787@gmail.com>
2022-08-19 10:20:03 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
 
-//configure express server
+//configure express servers
 
 const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
@@ -12,6 +12,8 @@ app.use(cors()); // Dont let local development give errors
 app.get("/", (req, res) => {
     res.json({ msg: "Welcome" });
 });
+const userRoute = require('./routes/userRoute')
+app.use('/users', userRoute)
 
 //listener for api calls
 app.listen(app.get("port"), () => {
